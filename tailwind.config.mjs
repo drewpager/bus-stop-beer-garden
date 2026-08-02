f1ee486d6ae7544b57b310b52f43e76656907bd6
drewpager <drew@greadings.com>
2026-08-02 13:59:13 -0700

    Font update: Overpass (Highway Gothic not directly available

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Overpass', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
       colors: {
         'bus-yellow': '#FCC200',
         'forest-green': '#2D5016',
