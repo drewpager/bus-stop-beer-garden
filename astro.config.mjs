ae4365e5b85c457a0e9d1c79c5780dea48b5b069
drewpager <drew@greadings.com>
2026-08-02 16:02:41 -0700

    Updating Apex Domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://www.beerbusstop.com',
+  site: 'https://beerbusstop.com',
   base: '/',
   integrations: [tailwind()],
 });
